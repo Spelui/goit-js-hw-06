8fb0cfca32da2f1f0b487198055eea19484690ee
spelui <vlad.spelui@gmail.com>
2021-11-09 19:26:31 +0200

    finish hw

--- js/task-10.js
+++ js/task-10.js
@@ -7,3 +7,35 @@ const controlsRef = document.querySelector("#controls");
 const input = controlsRef.querySelector("input");
 const create = controlsRef.querySelector("[data-create]");
 const destroy = controlsRef.querySelector("[data-destroy]");
+
+const boxes = document.querySelector("#boxes");
+
+create.addEventListener("click", getValue);
+
+destroy.addEventListener("click", clearResult);
+
+function getValue() {
+  const value = input.value;
+  if (!value) return;
+  creatBox(value);
+}
+
+function creatBox(value) {
+  const result = [];
+  let size = 30;
+  for (let i = 0; i < value; i += 1) {
+    const markup = `<div class="square" style="width:${size}px; height:${size}px; background:${getRandomHexColor()}"></div>`;
+    size += 10;
+    result.push(markup);
+  }
+  updateMarkup(result.join(""));
+}
+
+function updateMarkup(markup = "") {
+  boxes.innerHTML = markup;
+}
+
+function clearResult() {
+  creatBox(0);
+  input.value = "";
+}
